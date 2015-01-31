e4db80999239cfabc1dcd9681276c63da5739833
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-01-31 10:03:27 -0500

    use pypy

--- resources/supervisor/setup_supervisord.sh
+++ resources/supervisor/setup_supervisord.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y supervisor
 sudo service supervisor start
 
 cd /home/ubuntu
-sudo cp etl.conf /etc/supervisor/conf.d/
+sudo cp /home/ubuntu/TestLog-ETL/resources/supervisor/etl.conf /etc/supervisor/conf.d/
 
 sudo supervisorctl reread
 sudo supervisorctl update
