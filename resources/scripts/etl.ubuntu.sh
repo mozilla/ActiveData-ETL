4000b5e541334fe24cfc2efbc1e7abd9c69e7800
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-02-17 09:05:28 -0500

    fixes

--- resources/scripts/etl.ubuntu.sh
+++ resources/scripts/etl.ubuntu.sh
@@ -1,7 +1,8 @@
 cd /home/ubuntu/TestLog-ETL/
 git checkout etl
+git stash
 git pull origin etl
-
+git stash apply
 
 export PYTHONPATH=.
 python testlog_etl/etl.py --settings=resources/settings/etl_staging_settings.json &
