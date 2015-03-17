cddfcdacf36b27cdb631ff1e5d8e0d5f5ac7b2c4
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-03-17 14:21:56 -0400

    add setup script

---
+++ resources/scripts/setup_push_to_es.sh
@@ -0,0 +1,17 @@
+# USE THIS TO INSTALL INTO STANDARD EC2 INSTANCE
+sudo yum -y install python27
+mkdir  /home/ec2-user/temp
+cd  /home/ec2-user/temp
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python27 get-pip.py
+
+cd  /home/ec2-user
+sudo yum -y install git
+git clone https://github.com/klahnakoski/TestLog-ETL.git
+cd /home/ec2-user/TestLog-ETL/
+git checkout push-to-es
+sudo pip install -r requirements.txt
+
+cat > pulse_logger_staging_settings.json
+# PASTE SETTINGS FILE HERE
+# CTRL-D WHEN DONE
