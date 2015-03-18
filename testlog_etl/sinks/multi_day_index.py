fbacc0c4e57bf799f58ec0f78414f06df8565f3c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-03-18 10:57:32 -0400

    remove json manipulation for faster inserts

--- testlog_etl/sinks/multi_day_index.py
+++ testlog_etl/sinks/multi_day_index.py
@@ -7,7 +7,7 @@
 # Author: Kyle Lahnakoski (kyle@lahnakoski.com)
 #
 from __future__ import unicode_literals
-from pyLibrary import convert
+from pyLibrary import strings, convert
 from pyLibrary.aws.s3 import strip_extension
 from pyLibrary.debugs.logs import Log
 from pyLibrary.dot import nvl, wrap
@@ -32,11 +32,13 @@ class MultiDayIndex(object):
         self.indicies = {}  # MAP DATE (AS UNIX TIMESTAMP) TO INDEX
         self.es = elasticsearch.Alias(alias=settings.index, settings=settings)
         #FORCE AT LEAST ONE INDEX TO EXIST
-        dummy = wrap({"value": {"build": {"date": Date.now().unix}}})
+        dummy = wrap({"build": {"date": Date.now().unix}})
         self._get_queue(dummy)
 
     def _get_queue(self, d):
-        date = Date(nvl(d.value.build.date, d.value.run.timestamp)).floor(NEW_INDEX_INTERVAL)
+        date = Date(nvl(d.build.date, d.run.timestamp)).floor(NEW_INDEX_INTERVAL)
+        if not date:
+            Log.error("Can not get date from document")
         name = self.settings.index + "_" + date.format("%Y-%m-%d")
         uid = date.unix
 
@@ -75,26 +77,28 @@ class MultiDayIndex(object):
     def extend(self, documents):
         for d in wrap(documents):
             try:
-                queue = self._get_queue(d)
+                queue = self._get_queue(d.value)
                 queue.add(d)
             except Exception, e:
                 Log.error("Can not decide on index by build.date: {{doc|json}}", {"doc": d.value})
 
     def add(self, doc):
         d = wrap(doc)
-        queue = self._get_queue(doc)
+        queue = self._get_queue(doc.value)
         queue.add(doc)
 
     def copy(self, keys, source):
         num_keys = 0
+        queue = None  # PUT THE WHOLE BLOCK INTO SAME INDEX
         for key in keys:
-            records = []
             for line in source.read_lines(strip_extension(key)):
-                v = convert.json2value(line)
-                row = {"id": v._id, "value": v}
-                v._id = None
+                if queue is None:
+                    value = convert.json2value(line)
+                    queue = self._get_queue(value)
+                    row = {"id": value._id, "value": value}
+                else:
+                    _id = strings.between(line, "_id\": \"", "\"")  # AVOID
+                    row = {"id": _id, "json": line}
                 num_keys += 1
-                records.append(row)
-
-            self.extend(records)
+                queue.add(row)
         return num_keys
