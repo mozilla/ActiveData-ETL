8eefe3f82ba8d2c46c63fe459deb37aee8cc7c07
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-05-08 12:11:35 -0400

    fix the push to ES:  ids are string, or long, and ES can not handle that

--- testlog_etl/push_to_es.py
+++ testlog_etl/push_to_es.py
@@ -17,6 +17,7 @@ from pyLibrary.aws.s3 import strip_extension, key_prefix
 from pyLibrary.debugs import startup, constants
 from pyLibrary.debugs.logs import Log
 from pyLibrary.dot import wrap
+from pyLibrary.env import elasticsearch
 from pyLibrary.jsons import scrub
 from pyLibrary.maths import Math
 from pyLibrary.queries import qb
@@ -59,25 +60,7 @@ def diff(settings, please_stop=None):
     # EVERYTHING FROM ELASTICSEARCH
     es = MultiDayIndex(settings.elasticsearch, queue_size=100000)
 
-    result = es.search({
-        "aggs": {
-            "_match": {
-                "terms": {
-                    "field": "etl.source.source.id",
-                    "size": 0
-                }
-
-            }
-        }
-    })
-
-    good_es = []
-    for k in result.aggregations._match.buckets.key:
-        try:
-            good_es.append(int(k))
-        except Exception, e:
-            pass
-    in_es = set(good_es)
+    in_es = get_all_in_es(es)
 
     # EVERYTHING FROM S3
     bucket = s3.Bucket(settings.source)
@@ -132,6 +115,40 @@ def diff(settings, please_stop=None):
         please_stop_queue.put("STOP")
     for p in processes:
         p.join()
+
+
+def get_all_in_es(es):
+    in_es = set()
+
+    all_indexes = es.es.cluster.get_metadata().indices
+    for name, index in all_indexes.items():
+        if "unittest" not in index.aliases:
+            continue
+
+        result = elasticsearch.Index(index=name, alias="unittest", settings=es.es.settings).search({
+            "aggs": {
+                "_match": {
+                    "terms": {
+                        "field": "etl.source.source.id",
+                        "size": 200
+                    }
+
+                }
+            }
+        })
+
+        good_es = []
+        for k in result.aggregations._match.buckets.key:
+            try:
+                good_es.append(int(k))
+            except Exception, e:
+                pass
+        Log("got {{num}} from {{index}}", {
+            "num": len(good_es),
+            "index": name
+        })
+        in_es |= set(good_es)
+    return in_es
 
 
 def main():
