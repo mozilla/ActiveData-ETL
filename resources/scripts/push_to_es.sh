672d7d07702faefa574d58885aeddba4fadf6fe9
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-05-09 23:11:57 -0400

    script change

--- resources/scripts/push_to_es.sh
+++ resources/scripts/push_to_es.sh
@@ -3,5 +3,7 @@ cd /home/ec2-user/TestLog-ETL/
 git pull origin push-to-es
 export PYTHONPATH=.
 
-python27 testlog_etl/push_to_es.py --settings=resources/settings/push_to_es_staging_settings.json
+python27 testlog_etl/push_to_es.py --settings=resources/settings/push_to_es_staging_settings.json &
+disown -h
+
 
