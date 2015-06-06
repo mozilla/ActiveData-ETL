a7228e519aeda60d928159487c0310e04b5b709b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-06-06 13:16:02 -0400

    try this

--- resources/scripts/pulse_logger.sh
+++ resources/scripts/pulse_logger.sh
@@ -4,9 +4,12 @@ git pull origin pulse-logger
 export PYTHONPATH=.
 
 python27 testlog_etl/pulse_logger.py --settings=resources/settings/pulse_logger_staging_settings.json >& /dev/null < /dev/null &
+
+#nohup python27 testlog_etl/pulse_logger.py --settings=resources/settings/pulse_logger_staging_settings.json &
 disown -h
-tail -f  results/logs/pulse_logger.log
+
+tail -f  ~/TestLog-ETL/results/logs/pulse_logger.log
 
 # DO NOT HANG ONTO PROCESS (nohup)
-# nohup python27 testlog_etl/pulse_logger.py --settings=pulse_logger_dev_settings.json &
+#nohup python27 testlog_etl/pulse_logger.py --settings=resources/settings/pulse_logger_staging_settings.json &
 
