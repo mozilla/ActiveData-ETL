01998fcf9f043608b10de5775621e1ee803a970a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-06-15 00:49:28 -0400

    fix push to es

--- testlog_etl/push_to_es.py
+++ testlog_etl/push_to_es.py
@@ -42,7 +42,10 @@ def copy2es(es, settings, work_queue, please_stop=None):
             else:
                 sample_filter = None
 
-            more_keys = bucket.keys(prefix=key + ":")
+            if key.find(":")>=0:
+                more_keys = bucket.keys(prefix=key)
+            else:
+                more_keys = bucket.keys(prefix=key + ":")
             num_keys = es.copy(more_keys, bucket, sample_filter, settings.sample_size)
 
         if num_keys > 1:
