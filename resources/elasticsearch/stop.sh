3d3df400a47cc6d9fab1c7439b3873eb8189c00c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-07-29 16:09:13 -0400

    index aurora too

--- resources/elasticsearch/stop.sh
+++ resources/elasticsearch/stop.sh
@@ -2,6 +2,7 @@
 curl -XPUT localhost:9200/_cluster/settings -d '{"transient":{"cluster.routing.allocation.disable_allocation": true}}'
 
 curl -XPOST http://localhost:9200/_cluster/nodes/_local/_shutdown
+tail -f /data1/logs/active-data.log
 
 
 curl -XPOST http://localhost:9200/_shutdown
