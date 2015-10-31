b103f9ec315122e6a08301ac43b66cdcb19197ea
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-10-31 11:49:12 -0400

    fix import problem

--- pyLibrary/env/git.py
+++ pyLibrary/env/git.py
@@ -11,7 +11,6 @@
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
-import fabric
 
 from pyLibrary.env.processes import Process
 from pyLibrary.meta import cache
