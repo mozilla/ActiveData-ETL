abd212f6893f868b3925453c62c59fc019a4065c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-27 08:37:55 -0500

    fix

--- testlog_etl/update_push_to_es.py
+++ testlog_etl/update_push_to_es.py
@@ -104,7 +104,6 @@ def _refresh_indexer():
         result = run("git pull origin push-to-es")
         if result.find("Already up-to-date.") != -1:
             Log.note("No change required")
-            sudo("supervisorctl stop es")
         else:
             with fabric_settings(warn_only=True):
                 sudo("supervisorctl restart push_perf_to_es")
