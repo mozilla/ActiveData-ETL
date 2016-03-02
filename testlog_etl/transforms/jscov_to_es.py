9a41687341fe8ae719e011162f4793a17979a619
Trung Do <chin.bimbo@gmail.com>
2016-03-01 23:19:22 -0500

    Listen to pulse artifact messages directly

--- testlog_etl/transforms/jscov_to_es.py
+++ testlog_etl/transforms/jscov_to_es.py
@@ -13,20 +13,43 @@ import json
 
 from pyLibrary.dot import wrap
 from pyLibrary.env.git import get_git_revision
+from testlog_etl.transforms.pulse_block_to_es import scrub_pulse_record
+from testlog_etl.transforms import EtlHeadGenerator
+from pyLibrary.dot import Dict
+from pyLibrary.env import http
 
-DEBUG = True
 
 def process(source_key, source, destination, resources, please_stop=None):
-    with open(source) as json_file:
-        json_data = wrap(json.load(json_file))
-        output_lines = []
-        for i, obj in enumerate(json_data):
+    output = []
+    etl_header_gen = EtlHeadGenerator(source_key)
+
+    for i, line in enumerate(source.read_lines()):
+        stats = Dict()
+        pulse_record = scrub_pulse_record(source_key, i, line, stats)
+        artifact_file_name = pulse_record.artifact.name
+
+        # we're only interested in jscov files, at lease at the moment
+        if "jscov" not in artifact_file_name:
+            continue
+
+        # construct the artifact's full url
+        taskId = pulse_record.status.taskId
+        runId = pulse_record.runId
+        full_artifact_path = "https://public-artifacts.taskcluster.net/" + taskId + "/" + str(runId) + "/" + artifact_file_name
+
+        # fetch the artifact
+        response = http.get(full_artifact_path).all_content
+
+        # transform
+        json_data = wrap(json.loads(response))
+        for j, obj in enumerate(json_data):
             # get the test name. Just use the test file name at the moment
             # TODO: change this when needed
             last_slash_index = obj.testUrl.rfind("/")
             test_name = obj.testUrl[last_slash_index + 1:]
 
             for line in obj.covered:
+                dest_key, dest_etl = etl_header_gen.next(pulse_record.etl, j)
                 new_line = {
                     "test": {
                         "name": test_name,
@@ -36,11 +59,10 @@ def process(source_key, source, destination, resources, please_stop=None):
                         "sourceFile": obj.sourceFile,
                         "lineCovered": line
                     },
-                    "etl": {
-                        "type": "join",
-                        "revision": get_git_revision()
-                    }
+                    "etl": dest_key
                 }
-                output_lines.append(new_line)
+                # output_lines.append(new_line)
+                output.append(json.dumps(new_line))
 
-    return output_lines
+    # return output_lines
+    return output
