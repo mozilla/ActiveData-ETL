573f306120e2e9d221c0409ffc513b3c93f15a9a
Trung Do <chin.bimbo@gmail.com>
2016-03-06 22:05:52 -0500

    A much faster way to empty the bucket

--- testlog_etl/empty_codecoverage_bucket.py
+++ testlog_etl/empty_codecoverage_bucket.py
@@ -1,12 +1,10 @@
-from boto.s3.connection import S3Connection, Bucket
+import boto3 as boto3
 
 """
 This script will delete everything in the active-data-codecoverage-dev bucket
 """
 
-conn = S3Connection("", "")
-
-b = Bucket(conn, "active-data-codecoverage-dev")
-for x in b.list():
-    print("Deleting " + x.key)
-    b.delete_key(x.key)
+s3 = boto3.resource('s3')
+objects_to_delete = s3.meta.client.list_objects(Bucket="active-data-codecoverage-dev")
+delete_keys = {'Objects': [{'Key': k} for k in [obj['Key'] for obj in objects_to_delete.get('Contents', [])]]}
+s3.meta.client.delete_objects(Bucket="active-data-codecoverage-dev", Delete=delete_keys)
