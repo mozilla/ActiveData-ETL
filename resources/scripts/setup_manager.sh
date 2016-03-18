5168ba755447a66c9ad5508fc584af66d17964e4
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-03-18 13:47:04 -0400

    try to setup cron

--- resources/scripts/setup_manager.sh
+++ resources/scripts/setup_manager.sh
@@ -10,3 +10,9 @@ cd /
 sudo ln -s /home/ec2-user/logs logs
 
 cp ~/TestLog_ETL/resources/settings/monit.conf /etc/monit.conf
+
+
+# CRON JOBS
+sudo rm /var/spool/cron/ec2-user
+sudo cp /home/ec2-user/TestLog-ETL/resources/cron/cronfile.txt /var/spool/cron/ec2-user
+
