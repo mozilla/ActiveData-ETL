79dd7704c0b41d1495486374e22de0b3c2eee678
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-04-25 18:44:47 -0400

    updates to cron

--- resources/scripts/setup_manager.sh
+++ resources/scripts/setup_manager.sh
@@ -1,28 +1,54 @@
 #!/usr/bin/env bash
 
+# SPOT MANAGER REQUIRES THE FOLLOWING POLICY
+#{
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Effect": "Allow",
+#            "Action": [
+#                "ec2:*",
+#                "ses:*",
+#                "sqs:*",
+#                "iam:PassRole"
+#            ],
+#            "Resource": [
+#                "*"
+#            ]
+#        }
+#    ]
+#}
 
-sudo yum -y install python27
+
 sudo yum -y install git
-sudo yum install monit -y
 
-#INSTALL PYTHON
-mkdir  /home/ec2-user/temp
-cd  /home/ec2-user/temp
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python27 get-pip.py
+# INSTALL PYTHON
+sudo yum -y install python27
+sudo easy_install pip
 
-#INSTALL BUILDBOT IMPORT
+# INSTALL BUILDBOT IMPORT
 cd  /home/ec2-user
 git clone https://github.com/klahnakoski/TestLog-ETL.git
 cd /home/ec2-user/TestLog-ETL/
 git checkout manager
 
-#INSTALL SpotManager
+sudo /usr/local/bin/pip install -r requirements.txt
+
+# INSTALL SpotManager
 cd  /home/ec2-user
 git clone https://github.com/klahnakoski/SpotManager.git
 cd /home/ec2-user/SpotManager/
 git checkout manager
 
+sudo /usr/local/bin/pip install -r requirements.txt
+
+# INSTALL ActiveData
+cd  /home/ec2-user
+git clone https://github.com/klahnakoski/ActiveData.git
+cd /home/ec2-user/ActiveData/
+git checkout manager
+
+sudo /usr/local/bin/pip install -r requirements.txt
 
 
 
@@ -32,12 +58,20 @@ mkdir ~/logs/monit_emails
 cd /
 sudo ln -s /home/ec2-user/logs logs
 
-cp ~/TestLog_ETL/resources/settings/monit.conf /etc/monit.conf
+# COPY KEYS TO MACHINE
+#put ~/private_active_data_etl.json ~/private_active_data_etl.json
+cp ~/private_active_data_etl.json ~/private.json
+chmod 600 ~/private_active_data_etl.json
+chmod 600 ~/private.json
+
+#put ~/.ssh/aws-pulse-logger.pem ~/.ssh/aws-pulse-logger.pem
+chmod 600 ~/.ssh/aws-pulse-logger.pem
 
 # CRON JOBS
 chmod u+x /home/ec2-user/TestLog-ETL/resources/scripts/run_buildbot_json_logs.sh
 chmod u+x /home/ec2-user/SpotManager/examples/scripts/run_es.sh
 chmod u+x /home/ec2-user/SpotManager/examples/scripts/run_etl.sh
+chmod u+x /home/ec2-user/ActiveData/resources/scripts/run_codecoverage.sh
 
 sudo rm /var/spool/cron/ec2-user
 sudo cp /home/ec2-user/TestLog-ETL/resources/cron/manager.cron /var/spool/cron/ec2-user
