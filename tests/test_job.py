5025f8c790b683a0f57ce000450c0ff63408cc6a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-07-22 11:00:12 -0400

    fix import

--- tests/test_job.py
+++ tests/test_job.py
@@ -12,7 +12,7 @@ from __future__ import unicode_literals
 from pyLibrary.debugs.logs import Log
 from pyLibrary.env import http
 from pyLibrary.testing.fuzzytestcase import FuzzyTestCase
-from testlog_etl.transforms.pulse_block_to_job_logs import process_buildbot_log
+from activedata_etl.transforms.pulse_block_to_job_logs import process_buildbot_log
 
 false = False
 true = True
