67644715e3f5b08e332f0842582094c3904964de
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-11-07 05:43:45 -0500

    more logging

--- pyLibrary/aws/__init__.py
+++ pyLibrary/aws/__init__.py
@@ -131,6 +131,7 @@ def capture_termination_signal(please_stop):
         while not please_stop:
             try:
                 response = requests.get("http://169.254.169.254/latest/meta-data/spot/termination-time")
+                Log.note("terminate time? : {{code}}", code=response.status_code)
                 if response.status_code not in [400, 404]:
                     Log.warning("Shutdown AWS Spot Node {{name}} {{type}}", name=machine_metadata.name, type=machine_metadata.aws_instance_type)
                     please_stop.go()
