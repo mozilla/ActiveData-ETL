d1bcb5bf1e30f0873fe0c5b9585231537312df96
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-07-31 12:59:59 -0400

    do not process gcda

--- activedata_etl/transforms/cov_to_es.py
+++ activedata_etl/transforms/cov_to_es.py
@@ -89,21 +89,21 @@ def process(source_key, source, destination, resources, please_stop=None):
                         artifact_etl,
                         please_stop
                     ))
-                elif "gcda" in artifact.name:
-                    coverage_artifact_exists = True
-                    _, artifact_etl = etl_header_gen.next(source_etl=parent_etl, url=artifact.url)
-                    if DEBUG:
-                        Log.note("Processing gcda artifact: {{url}}", url=artifact.url)
-
-                    keys.extend(process_gcda_artifact(
-                        source_key,
-                        resources,
-                        destination,
-                        artifact,
-                        task_cluster_record,
-                        artifact_etl,
-                        please_stop
-                    ))
+                # elif "gcda" in artifact.name:
+                #     coverage_artifact_exists = True
+                #     _, artifact_etl = etl_header_gen.next(source_etl=parent_etl, url=artifact.url)
+                #     if DEBUG:
+                #         Log.note("Processing gcda artifact: {{url}}", url=artifact.url)
+                #
+                #     keys.extend(process_gcda_artifact(
+                #         source_key,
+                #         resources,
+                #         destination,
+                #         artifact,
+                #         task_cluster_record,
+                #         artifact_etl,
+                #         please_stop
+                #     ))
             except Exception as e:
                 Log.error(TRY_AGAIN_LATER, reason="problem processing " + artifact.url + " for key " + source_key, cause=e)
 
