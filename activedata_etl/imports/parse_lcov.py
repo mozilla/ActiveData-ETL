c4ccb986b093665c42652b186fa1d77b2b1325a3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-18 12:41:13 -0400

    send warnings for large coverage

--- activedata_etl/imports/parse_lcov.py
+++ activedata_etl/imports/parse_lcov.py
@@ -41,6 +41,8 @@ def parse_lcov_coverage(stream):
 
         if line == 'end_of_record':
             for source in coco_format(current_source):
+                if source.total_covered>10000:
+                    Log.warning("{{name}} has {{num}} lines covered", name=source.file.name, num=source.total_covered)
                 if EMIT_RECORDS_WITH_ZERO_COVERAGE:
                     yield source
                 elif source.file.total_covered:
