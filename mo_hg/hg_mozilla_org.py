ff14d9ab14900ace3c96a40b0893cc455c8128c3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-31 17:29:32 -0400

    better warning

--- mo_hg/hg_mozilla_org.py
+++ mo_hg/hg_mozilla_org.py
@@ -456,10 +456,11 @@ class HgMozillaOrg(object):
                 diff = response.content.decode("utf8", "replace")
                 json_diff = diff_to_json(diff)
                 num_changes = jx.count(c for f in json_diff for c in f.changes)
-                if json_diff and num_changes < MAX_DIFF_SIZE:
-                    return json_diff
-                else:
-                    Log.warning("Revision at {{url}} has a diff with {{num}} changes, ignored", url=url, num=num_changes)
+                if json_diff:
+                    if num_changes < MAX_DIFF_SIZE:
+                        return json_diff
+                    else:
+                        Log.warning("Revision at {{url}} has a diff with {{num}} changes, ignored", url=url, num=num_changes)
             except Exception as e:
                 Log.warning("could not get unified diff", cause=e)
 
