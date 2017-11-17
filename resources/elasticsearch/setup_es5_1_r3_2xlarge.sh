88ab01e29539e89de308f04f53aa6edab5103804
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-11-17 10:00:52 -0500

    fix

--- resources/elasticsearch/setup_es5_1_r3_2xlarge.sh
+++ resources/elasticsearch/setup_es5_1_r3_2xlarge.sh
@@ -109,6 +109,7 @@ cd ~/ActiveData-ETL/
 git pull origin push-to-es5
 sudo cp ~/ActiveData-ETL/resources/elasticsearch/elasticsearch5_1.yml /usr/local/elasticsearch/config/elasticsearch.yml
 sudo cp ~/ActiveData-ETL/resources/elasticsearch/jvm.options /usr/local/elasticsearch/config/jvm.options
+sudo cp ~/ActiveData-ETL/resources/elasticsearch/log4j2.properties /usr/local/elasticsearch/config/log4j2.properties
 sudo chown -R ec2-user:ec2-user /usr/local/elasticsearch
 
 
@@ -150,6 +151,7 @@ cd ~/ActiveData-ETL/
 git pull origin push-to-es5
 sudo cp ~/ActiveData-ETL/resources/elasticsearch/elasticsearch5_1.yml /usr/local/elasticsearch/config/elasticsearch.yml
 sudo cp ~/ActiveData-ETL/resources/elasticsearch/jvm.options /usr/local/elasticsearch/config/jvm.options
+sudo cp ~/ActiveData-ETL/resources/elasticsearch/log4j2.properties /usr/local/elasticsearch/config/log4j2.properties
 sudo chown -R ec2-user:ec2-user /usr/local/elasticsearch
 cd /usr/local/elasticsearch
 bin/elasticsearch
