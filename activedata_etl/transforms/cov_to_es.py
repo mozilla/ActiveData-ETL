ba672753055881fa0118648cb10566f756d2ebea
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-11-28 07:55:51 -0500

    enable grcov

--- activedata_etl/transforms/cov_to_es.py
+++ activedata_etl/transforms/cov_to_es.py
@@ -11,6 +11,7 @@ from __future__ import unicode_literals
 
 from activedata_etl.imports.task import minimize_task
 from activedata_etl.transforms import EtlHeadGenerator, TRY_AGAIN_LATER
+from activedata_etl.transforms.grcov_to_es import process_grcov_artifact
 from activedata_etl.transforms.jsvm_to_es import process_jsvm_artifact
 from mo_json import json2value
 from mo_logs import Log
@@ -75,24 +76,24 @@ def process(source_key, source, destination, resources, please_stop=None):
                     # ))
                 elif "grcov" in artifact.name:
                     pass
-                    # if not task_cluster_record.repo.push.date:
-                    #     Log.warning("expecting a repo.push.date for all tasks source_key={{key}}", key=source_key)
-                    #     continue
-                    #
-                    # coverage_artifact_exists = True
-                    # _, artifact_etl = etl_header_gen.next(source_etl=parent_etl, url=artifact.url)
-                    # if DEBUG:
-                    #     Log.note("Processing grcov artifact: {{url}}", url=artifact.url)
-                    #
-                    # keys.extend(process_grcov_artifact(
-                    #     source_key,
-                    #     resources,
-                    #     destination,
-                    #     artifact,
-                    #     task_cluster_record,
-                    #     artifact_etl,
-                    #     please_stop
-                    # ))
+                    if not task_cluster_record.repo.push.date:
+                        Log.warning("expecting a repo.push.date for all tasks source_key={{key}}", key=source_key)
+                        continue
+
+                    coverage_artifact_exists = True
+                    _, artifact_etl = etl_header_gen.next(source_etl=parent_etl, url=artifact.url)
+                    if DEBUG:
+                        Log.note("Processing grcov artifact: {{url}}", url=artifact.url)
+
+                    keys.extend(process_grcov_artifact(
+                        source_key,
+                        resources,
+                        destination,
+                        artifact,
+                        task_cluster_record,
+                        artifact_etl,
+                        please_stop
+                    ))
                 elif "jsvm" in artifact.name:
                     if not task_cluster_record.repo.push.date:
                         Log.warning("expecting a repo.push.date for all tasks source_key={{key}}", key=source_key)
