5bd2c747ee35aab493d4117b41ba2d71c1186c91
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-04-09 13:45:37 -0400

    fix requirements, fix pythonpath

--- resources/scripts/push_to_es.sh
+++ resources/scripts/push_to_es.sh
@@ -2,7 +2,7 @@ cd /home/ec2-user/ActiveData-ETL/
 
 
 git pull origin push-to-es6
-export PYTHONPATH=.
+export PYTHONPATH=.:vendor
 
 python27 activedata_etl/push_to_es.py --settings=resources/settings/staging/push_to_es.json
 
