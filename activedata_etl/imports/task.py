b983cfe9bbc832a8ce84aca7019464f3d1a45df3
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-10-01 10:50:25 -0400

    another test

--- activedata_etl/imports/task.py
+++ activedata_etl/imports/task.py
@@ -294,6 +294,7 @@ TEST_SUITE = {
         "raptor-tp6-firefox",
         "raptor-unity-webgl-chrome",
         "raptor-unity-webgl-firefox",
+        "raptor-unity-webgl-geckoview",
         "raptor-wasm-misc-chrome",
         "raptor-wasm-misc-firefox",
         "raptor-wasm-misc-baseline-firefox",
