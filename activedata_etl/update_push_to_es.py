01bf78d6635ad4bf65cf13c01f3f24c6664d052e
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-11-19 13:45:43 -0500

    first attempt

--- activedata_etl/update_push_to_es.py
+++ activedata_etl/update_push_to_es.py
@@ -9,15 +9,15 @@
 from __future__ import division
 from __future__ import unicode_literals
 
-import logging
-
 from boto import ec2 as boto_ec2
 
+from jx_python import jx
 from mo_collections import UniqueIndex
 from mo_dots import unwrap, wrap
 from mo_dots.objects import datawrap, DataObject
 from mo_fabric import Connection
 from mo_logs import Log, startup, constants
+from mo_threads import Thread
 from pyLibrary.aws import aws_retry
 
 
@@ -42,10 +42,8 @@ def _get_managed_instances(ec2_conn, name):
 
 
 def _disable_oom_on_es(conn):
-
     with conn.warn_only():
         conn.sudo("supervisorctl start es")
-
 
     with conn.cd("/home/ec2-user"):
         conn.run("mkdir -p temp")
@@ -66,23 +64,37 @@ def _disable_oom_on_es(conn):
         conn.sudo("sudo cp oom_adj /proc/" + pid + "/oom_adj")
 
 
-def _refresh_indexer(config, instance):
+def _refresh_indexer(config, instance, please_stop):
+    Log.note(
+        "Reset {{instance_id}} ({{name}}) at {{ip}}",
+        instance_id=instance.id,
+        name=instance.tags["Name"],
+        ip=instance.ip_address
+    )
+    try:
+        with Connection(config, host=instance.ip_address) as conn:
+            with conn.cd("/usr/local/elasticsearch"):
+                conn.sudo("rm -f java*.hprof")
 
-    with Connection(config) as conn:
-        with conn.cd("/usr/local/elasticsearch"):
-            conn.sudo("rm -f java*.hprof")
-
-        _disable_oom_on_es(conn)
-        with conn.cd("/home/ec2-user/ActiveData-ETL/"):
-            result = conn.run("git pull origin push-to-es6")
-            if "Already up-to-date." in result:
-                Log.note("No change required")
-            else:
-                # RESTART ANYWAY, SO WE USE LATEST INDEX
-                conn.run("~/pypy/bin/pypy -m pip install -r requirements.txt")
-                with conn.warn_only():
-                    conn.sudo("supervisorctl stop push_to_es:*")
-                    conn.sudo("supervisorctl start push_to_es:00")
+            _disable_oom_on_es(conn)
+            with conn.cd("/home/ec2-user/ActiveData-ETL/"):
+                result = conn.run("git pull origin push-to-es6")
+                if "Already up-to-date." in result:
+                    Log.note("No change required")
+                else:
+                    # RESTART ANYWAY, SO WE USE LATEST INDEX
+                    conn.run("~/pypy/bin/pypy -m pip install -r requirements.txt")
+                    with conn.warn_only():
+                        conn.sudo("supervisorctl stop push_to_es:*")
+                        conn.sudo("supervisorctl start push_to_es:00")
+    except Exception as e:
+        Log.warning(
+            "could not refresh {{instance_id}} ({{name}}) at {{ip}}",
+            instance_id=instance.id,
+            name=instance.tags["Name"],
+            ip=instance.ip_address,
+            cause=e
+        )
 
 
 def _start_supervisor(conn):
@@ -102,49 +114,26 @@ def main():
         constants.set(settings.constants)
         Log.start(settings.debug)
 
-        logging.getLogger('paramiko.transport').addHandler(LogTranslate())
-
         aws_args = dict(
             region_name=settings.aws.region,
             aws_access_key_id=unwrap(settings.aws.aws_access_key_id),
             aws_secret_access_key=unwrap(settings.aws.aws_secret_access_key)
         )
         ec2_conn = boto_ec2.connect_to_region(**aws_args)
-
         instances = _get_managed_instances(ec2_conn, settings.name)
 
-        for i in instances:
-            try:
-                Log.note("Reset {{instance_id}} ({{name}}) at {{ip}}", instance_id=i.id, name=i.tags["Name"], ip=i.ip_address)
-                # _config_fabric(settings.fabric, i)
-                _refresh_indexer(i)
-            except Exception as e:
-                Log.warning(
-                    "could not refresh {{instance_id}} ({{name}}) at {{ip}}",
-                    instance_id=i.id,
-                    name=i.tags["Name"],
-                    ip=i.ip_address,
-                    cause=e
-                )
+        for g, ii in jx.groupby(instances, size=1):
+            threads = [
+                Thread.run(i.name, _refresh_indexer, settings.fabric, i)
+                for i in ii
+            ]
+
+            for t in threads:
+                t.join()
     except Exception as e:
         Log.error("Problem with etl", e)
     finally:
         Log.stop()
-
-
-class LogTranslate(object):
-
-    def __init__(self, level=0):
-        self.level=level
-
-    def emit(self, record):
-        Log.note("{{record}}", record=record)
-
-    def flush(self):
-        pass
-
-    def handle(self, record):
-        Log.note("{{record|json}}", record=DataObject(record))
 
 
 if __name__ == "__main__":
