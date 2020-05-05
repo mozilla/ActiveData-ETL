c31bdbd444173e40815b2f59315d01d2418e46e3
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-05-05 12:50:24 -0400

    contact

--- tests/test_gcov.py
+++ tests/test_gcov.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Author: Kyle Lahnakoski (klahnakoski@mozilla.com)
+# Contact: Kyle Lahnakoski (klahnakoski@mozilla.com)
 #
 from __future__ import division
 from __future__ import unicode_literals
